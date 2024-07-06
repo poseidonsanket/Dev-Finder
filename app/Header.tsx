d6ab1d80686ce01dc988884cef95052cca498c1c
Sanket Dadali <sanketdadali05@gmail.com>
2024-07-06 15:39:38 +0530

    finish setting up next-auth

---
+++ app/Header.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export function Header() {
+  const session = useSession();
+  return (
+    <header>
+      <div>
+        {session?.data ? (
+          <Button onClick={() => signOut()}>SignOut</Button>
+        ) : (
+          <Button onClick={() => signIn("google")}>SignIn</Button>
+        )}
+        <ModeToggle />
+      </div>
+    </header>
+  );
+}
