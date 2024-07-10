0c7625b0b1f9f9e6d68c56f07245ed66341e43c9
Sanket Dadali <sanketdadali05@gmail.com>
2024-07-10 21:30:11 +0530

    bug fixes

--- app/create-room/Form.tsx
+++ app/create-room/Form.tsx
@@ -20,8 +20,8 @@ import { useRouter } from "next/navigation";
 const formSchema = z.object({
   name: z.string().min(1).max(50),
   description: z.string().min(1).max(250),
-  githubRepo: z.string().min(1).max(50),
-  tags: z.string().min(1).max(50),
+  githubRepo: z.string().min(1).max(100),
+  tags: z.string().min(1).max(100),
 });
 
 export function CreateRoomForm() {
